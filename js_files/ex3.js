044c7dbd0f24665d7f2df0f5410fd092628e6eda
Kalimeris Spyros <kalimerisspyros@gmail.com>
2022-02-02 10:47:17 +0200

    working with arrays

--- js_files/ex3.js
+++ js_files/ex3.js
@@ -1,2 +1,29 @@
-"user strict"; 
+"use strict"; 
 
+console.log(calculateAge(1992));
+
+// Normal function
+
+//calculated the age depending on birth date
+function calculateAge(year){
+    return 2022 - year;
+}
+
+// this will cause an error cause we cant call an expression function
+// before initialization
+
+
+let calculateAge2 = function (year){
+    return 2022 - year;
+}
+
+
+console.log(calculateAge2(1995));
+
+//Arrow functions
+
+
+
+let calculateAge3 = (year) => 2022 - year;
+// again we call the arrow function before initialization
+console.log(calculateAge3(1999));
